189f2059c4615566b60f630a1088d6a1ba9de103
Nahomie <nducasse3@gmail.com>
2018-01-19 13:56:16 +0000

    Done.

--- array.js
+++ array.js
@@ -1,9 +1,10 @@
 function addElementToBeginningOfArray(array, element) {
- // add the element to the beginning of the array without mutating the array
+return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  // add element to the beginning of the array by mutating the array
+ array.unshift(element);
+ return array
 }
 
 function addElementToEndOfArray(array, element) {
